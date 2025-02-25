cdd802affa18fd403edd3d360ecbb9328c03bf5a
visheshsarvaiya <visheshsarvaiya0@gmail.com>
2025-02-26 00:26:56 +0530

    util code discussion

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error{  //api error ke liye
+    constructor(
+        statusCode,
+        message= " Something went wrong ",  // error pata chal jati hai
+        error=[],
+        statck=""
+    ) {   //override kar rahe hai
+
+      super(message)
+      this.statusCode=statusCode
+      this.data=null
+      this.message=message
+      this.success=false;
+      this.errors=errors
+
+   if(statck){
+    this.stack=statck
+   }
+   else{
+    Error.captureStackTrace(this,this.constructor)
+   }
+    }
+}
